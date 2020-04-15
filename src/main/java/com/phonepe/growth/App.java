26352b340882ee1387538838745989caaf1506bc
Mohammed Irfanulla <mohammed.irfanulla@phonepe.com>
2020-04-15 10:35:53 +0530

    Initial commit

---
+++ src/main/java/com/phonepe/growth/App.java
@@ -0,0 +1,13 @@
+package com.phonepe.growth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
