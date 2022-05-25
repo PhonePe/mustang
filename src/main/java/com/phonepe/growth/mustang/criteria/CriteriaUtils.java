8de56f1c55e6d741b2383e95ff7a3fa2eabc9ff3
Sidharth <sidharth.sidharth@phonepe.com>
2022-05-26 00:26:06 +0530

    Added criteria conversion utility

---
+++ src/main/java/com/phonepe/growth/mustang/criteria/CriteriaUtils.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2022 Mohammed Irfanulla S <mohammed.irfanulla.s1@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phonepe.growth.mustang.criteria;
+
+import com.phonepe.growth.mustang.composition.Composition;
+import com.phonepe.growth.mustang.composition.impl.Conjunction;
+import com.phonepe.growth.mustang.composition.impl.Disjunction;
+import com.phonepe.growth.mustang.criteria.impl.CNFCriteria;
+import com.phonepe.growth.mustang.criteria.impl.DNFCriteria;
+import com.phonepe.growth.mustang.predicate.Predicate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CriteriaUtils {
+
+    public static CNFCriteria getCNFCriteria(DNFCriteria dnfCriteria) {
+        return CNFCriteria.builder()
+                .id(dnfCriteria.getId())
+                .disjunctions(getCompositions(dnfCriteria.getConjunctions(), predicates -> Disjunction.builder()
+                        .predicates(predicates)
+                        .build()))
+                .build();
+    }
+
+    public static DNFCriteria getDNFCriteria(CNFCriteria cnfCriteria) {
+        return DNFCriteria.builder()
+                .id(cnfCriteria.getId())
+                .conjunctions(getCompositions(cnfCriteria.getDisjunctions(), predicates -> Conjunction.builder()
+                        .predicates(predicates)
+                        .build()))
+                .build();
+    }
+
+    private static <T> List<T> getCompositions(List<? extends Composition> compositions,
+                                               Function<List<Predicate>, T> transformingFunction) {
+        // Separating out multi predicate compositions and single predicate compositions
+        List<Predicate> commonPredicates = new ArrayList<>();
+        List<Composition> multiPredicateCompositions = new ArrayList<>();
+        for (Composition composition : compositions) {
+            if (composition.getPredicates()
+                    .size() == 1) {
+                commonPredicates.add(composition.getPredicates()
+                        .get(0));
+            } else if (composition.getPredicates()
+                    .size() > 1) {
+                multiPredicateCompositions.add(composition);
+            }
+        }
+
+        // If there are no multi predicate composition, just create a single composition with all predicates
+        if (multiPredicateCompositions.size() == 0) {
+            return new ArrayList<>(Collections.singletonList(transformingFunction.apply(commonPredicates)));
+        }
+
+        List<T> resultantCompositions = new ArrayList<>();
+
+        int[] predicateIndex = new int[multiPredicateCompositions.size()];
+        int predicateIndexUpdater = multiPredicateCompositions.size() - 1;
+        while (predicateIndexUpdater != -1) {
+            // Taking one predicate from each composition according to predicateIndex
+            List<Predicate> predicates = IntStream.range(0, multiPredicateCompositions.size())
+                    .mapToObj(compositionNumber -> multiPredicateCompositions.get(compositionNumber)
+                            .getPredicates()
+                            .get(predicateIndex[compositionNumber]))
+                    .collect(Collectors.toList());
+            // Adding all the common predicates
+            predicates.addAll(commonPredicates);
+
+            resultantCompositions.add(transformingFunction.apply(predicates));
+
+            // updating predicateIndex
+            predicateIndexUpdater = multiPredicateCompositions.size() - 1;
+            while (predicateIndexUpdater >= 0
+                    && predicateIndex[predicateIndexUpdater] + 1 == multiPredicateCompositions.get(
+                            predicateIndexUpdater)
+                    .getPredicates()
+                    .size()) {
+                predicateIndex[predicateIndexUpdater] = 0;
+                predicateIndexUpdater--;
+            }
+            if (predicateIndexUpdater != -1) {
+                predicateIndex[predicateIndexUpdater]++;
+            }
+        }
+        return resultantCompositions;
+    }
+}
---
+++ src/test/java/com/phonepe/growth/mustang/criteria/CriteriaUtilsTest.java
@@ -0,0 +1,127 @@
+package com.phonepe.growth.mustang.criteria;
+
+import com.google.common.collect.Sets;
+import com.phonepe.growth.mustang.composition.impl.Conjunction;
+import com.phonepe.growth.mustang.composition.impl.Disjunction;
+import com.phonepe.growth.mustang.criteria.impl.CNFCriteria;
+import com.phonepe.growth.mustang.criteria.impl.DNFCriteria;
+import com.phonepe.growth.mustang.predicate.Predicate;
+import com.phonepe.growth.mustang.predicate.impl.IncludedPredicate;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CriteriaUtilsTest {
+
+    private static final String CRITERIA_ID = "TEST_CRITERIA_ID";
+
+    private Predicate PREDICATE_A = IncludedPredicate.builder()
+            .lhs("$.a")
+            .values(Sets.newHashSet("A1", "A2"))
+            .build();
+
+    private Predicate PREDICATE_B = IncludedPredicate.builder()
+            .lhs("$.b")
+            .values(Sets.newHashSet("B1", "B2"))
+            .build();
+
+    private Predicate PREDICATE_C = IncludedPredicate.builder()
+            .lhs("$.c")
+            .values(Sets.newHashSet("C1", "C2"))
+            .build();
+
+    private Predicate PREDICATE_D = IncludedPredicate.builder()
+            .lhs("$.d")
+            .values(Sets.newHashSet("D1", "D2"))
+            .build();
+
+    private Predicate PREDICATE_E = IncludedPredicate.builder()
+            .lhs("$.e")
+            .values(Sets.newHashSet("E1", "E2"))
+            .build();
+
+    @Test
+    public void testGetDNFCriteria() {
+        // (A OR B) AND (C OR D) AND E
+        CNFCriteria cnfCriteria = CNFCriteria.builder()
+                .id(CRITERIA_ID)
+                .disjunction(Disjunction.builder()
+                        .predicate(PREDICATE_A)
+                        .predicate(PREDICATE_B)
+                        .build())
+                .disjunction(Disjunction.builder()
+                        .predicate(PREDICATE_C)
+                        .predicate(PREDICATE_D)
+                        .build())
+                .disjunction(Disjunction.builder()
+                        .predicate(PREDICATE_E)
+                        .build())
+                .build();
+        // (A AND C AND E) OR (A AND D AND E) OR (B AND C AND E) OR (B AND D AND E)
+        DNFCriteria expectedDNFCriteria = DNFCriteria.builder()
+                .id(CRITERIA_ID)
+                .conjunction(Conjunction.builder()
+                        .predicate(PREDICATE_A)
+                        .predicate(PREDICATE_C)
+                        .predicate(PREDICATE_E)
+                        .build())
+                .conjunction(Conjunction.builder()
+                        .predicate(PREDICATE_A)
+                        .predicate(PREDICATE_D)
+                        .predicate(PREDICATE_E)
+                        .build())
+                .conjunction(Conjunction.builder()
+                        .predicate(PREDICATE_B)
+                        .predicate(PREDICATE_C)
+                        .predicate(PREDICATE_E)
+                        .build())
+                .conjunction(Conjunction.builder()
+                        .predicate(PREDICATE_B)
+                        .predicate(PREDICATE_D)
+                        .predicate(PREDICATE_E)
+                        .build())
+                .build();
+        DNFCriteria actualDNFCriteria = CriteriaUtils.getDNFCriteria(cnfCriteria);
+        Assert.assertEquals(expectedDNFCriteria, actualDNFCriteria);
+    }
+
+    @Test
+    public void testGetCNFCriteria() {
+        // (A AND B AND C) OR D OR E
+        DNFCriteria dnfCriteria = DNFCriteria.builder()
+                .id(CRITERIA_ID)
+                .conjunction(Conjunction.builder()
+                        .predicate(PREDICATE_A)
+                        .predicate(PREDICATE_B)
+                        .predicate(PREDICATE_C)
+                        .build())
+                .conjunction(Conjunction.builder()
+                        .predicate(PREDICATE_D)
+                        .build())
+                .conjunction(Conjunction.builder()
+                        .predicate(PREDICATE_E)
+                        .build())
+                .build();
+        // (A OR D OR E) AND (B OR D OR E) AND (C OR D OR E)
+        CNFCriteria expectedCNFCriteria = CNFCriteria.builder()
+                .id(CRITERIA_ID)
+                .disjunction(Disjunction.builder()
+                        .predicate(PREDICATE_A)
+                        .predicate(PREDICATE_D)
+                        .predicate(PREDICATE_E)
+                        .build())
+                .disjunction(Disjunction.builder()
+                        .predicate(PREDICATE_B)
+                        .predicate(PREDICATE_D)
+                        .predicate(PREDICATE_E)
+                        .build())
+                .disjunction(Disjunction.builder()
+                        .predicate(PREDICATE_C)
+                        .predicate(PREDICATE_D)
+                        .predicate(PREDICATE_E)
+                        .build())
+                .build();
+        CNFCriteria actualCNFCriteria = CriteriaUtils.getCNFCriteria(dnfCriteria);
+        Assert.assertEquals(expectedCNFCriteria, actualCNFCriteria);
+    }
+
+}
