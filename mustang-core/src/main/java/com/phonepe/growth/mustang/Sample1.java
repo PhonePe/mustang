27372357c9ddf2fd488f7b5597f480a0dca028bd
Mohammed Irfanulla <mohammed.irfanulla@phonepe.com>
2025-09-12 19:12:00 +0530

    removed files

--- mustang-core/src/main/java/com/phonepe/growth/mustang/Sample1.java
+++
@@ -1,7 +0,0 @@
-package com.phonepe.growth.mustang;
-
-public class Sample1 {
-    public static void main(String[] args) {
-        System.out.println(String.format("%32s", Integer.toBinaryString(127)).replace(" ","0"));
-    }
-}
